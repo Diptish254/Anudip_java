ed91fabee123024c868cbdd8a6fc8d4e8432b9ce
admin <admin@Diptish>
2022-09-07 12:18:02 +0530

    for loop

---
+++ for loop/src/TwoTable.java
@@ -0,0 +1,14 @@
+
+public class TwoTable {
+
+	public static void main(String[] args) {
+		int num = 2;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.println(num*i);
+        }
+}
+
+	}
+
+
