8d9e758c3ef2881356e71f89fdcc514e59cfda02
Diptish Bandu Kakad <113004413+Diptish254@users.noreply.github.com>
2022-10-14 10:06:43 +0530

    Update InvalidMailIdException.java
    lab 6

--- lab_test_6/src/lab_test_6/InvalidMailIdException.java
+++ lab_test_6/src/lab_test_6/InvalidMailIdException.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class InvalidMailIdException {
 
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter Emain Id :");
-		String id=sc.next();
+	public static void main(String[] args) {                                
+		Scanner sc=new Scanner(System.in);            //  create scanner  to give  user input  
+		System.out.println("Enter Emain Id :");             
+		String id=sc.next();                        //store the  email id
 		try {
-		boolean b= (id.contains(".") && id.contains("@") && id.endsWith("com")
+		boolean b= (id.contains(".") && id.contains("@") && id.endsWith("com")     // check the email id formate 
 					|| id.contains(" "));
-		if(b==false) 
+		if(b==false)                                               // through IllegalArgumentException 
 		throw new IllegalArgumentException();
 		System.out.println("Valid Email.");
 		}
