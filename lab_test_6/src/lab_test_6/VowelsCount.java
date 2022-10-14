f065a65905352b32d86680709c2674ca4359684b
Diptish Bandu Kakad <113004413+Diptish254@users.noreply.github.com>
2022-10-14 10:38:03 +0530

    Update VowelsCount.java
    lab 6

--- lab_test_6/src/lab_test_6/VowelsCount.java
+++ lab_test_6/src/lab_test_6/VowelsCount.java
@@ -4,34 +4,35 @@ import java.util.Scanner;
 
 public class VowelsCount {
 	
-	    public static int countVowels()
+	    public static int countVowels()                       // create a method countVowels
 	    {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter The string :");
-		String s1=sc.next();
-		char ch[]=s1.toCharArray();
+		Scanner sc=new Scanner(System.in);                                   
+		System.out.println("Enter The string :");          
+		String s1=sc.next();                             // store the    user value                                               
+		char ch[]=s1.toCharArray();                      //   conver the steing into array 
 	    int count=0;
-	    for(int i=0;i<ch.length;i++) 
+	    for(int i=0;i<ch.length;i++)                                  //create loop for array 
 	    {
-		if(ch[i]=='a'||ch[i]=='e'||ch[i]=='i'||ch[i]=='o'||ch[i]=='u') {
+		if(ch[i]=='a'||ch[i]=='e'||ch[i]=='i'||ch[i]=='o'||ch[i]=='u')      //if condition to check  vowels are present or not 
+		{
 		count++;
 	    }
 		}
 	    
 	    
-	    try {
-	    	if(s1.contains("x"))
+	    try {                                                 //       if   string contain x then through  exception 
+	    	if(s1.contains("x"))                       
 	    	throw new Exception();
 	        }
 	    catch(Exception e) 
 	        {
 	    	System.out.println("String should not conatin x");
 	        }
-	    return count;
+	    return count;                                             // return vowel count 
 	       }
 	
 	public static void main(String[] args) {  
-		System.out.println("Number of vowels in the String :"+countVowels());
+		System.out.println("Number of vowels in the String :"+countVowels());          // enter the string and call the method             
 	     }
 
          }
